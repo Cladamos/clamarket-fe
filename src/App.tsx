624a3b111bafacd1078bb224f6d18e3977af1087
cladamos <caliskangokberk@gmail.com>
2026-04-21 15:32:47 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@mantine/core"
+
+export function App() {
+  return (
+    <div className="flex flex-col justify-center items-center py-20">
+      <p className="text-3xl">clamarket</p>
+      <Button>Button</Button>
+    </div>
+  )
+}
